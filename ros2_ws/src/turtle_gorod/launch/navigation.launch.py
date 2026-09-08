784319acdbf3df5154ab8d05cffd96a4d8ef2850
Alex <80409749+xander-07@users.noreply.github.com>
2026-09-08 17:19:55 +0300

    Add Nav2 launch file

---
+++ ros2_ws/src/turtle_gorod/launch/navigation.launch.py
@@ -0,0 +1,33 @@
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+import os
+
+
+def generate_launch_description():
+    turtle_share = get_package_share_directory("turtle_gorod")
+    nav2_share = get_package_share_directory("nav2_bringup")
+
+    map_yaml = LaunchConfiguration("map")
+    params = os.path.join(turtle_share, "config", "nav2_params.yaml")
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            "map",
+            description="Absolute path to a Nav2 map yaml file",
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(nav2_share, "launch", "bringup_launch.py")
+            ),
+            launch_arguments={
+                "map": map_yaml,
+                "params_file": params,
+                "use_sim_time": "false",
+                "autostart": "true",
+                "use_composition": "false",
+            }.items(),
+        ),
+    ])
